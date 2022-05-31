64e93dec6d7089ed8210d92382f9e119340d9e59
paranoidAndroid490 <mrcsoda@Gmail.com>
2022-05-31 17:49:55 +0200

    Add - inheritance and polymorphism excercise

--- esercizi-ts/scripts/script.js
+++ esercizi-ts/scripts/script.js
@@ -22,24 +22,29 @@ var operationsHandler = function (operand) {
     var firstNum = prompt("Please, insert the first number");
     var secondNum = prompt("Please, insert the second number ");
     var operation = new Operation(+firstNum, +secondNum);
-    if (operand === 'sum') {
-        document.getElementById('resultSum').innerHTML = "" + operation.sum();
+    if (operand === "sum") {
+        document.getElementById("resultSum").innerHTML = "" + operation.sum();
     }
-    if (operand === 'difference') {
-        document.getElementById('resultDifference').innerHTML = "" + operation.diff();
+    if (operand === "difference") {
+        document.getElementById("resultDifference").innerHTML =
+            "" + operation.diff();
     }
-    if (operand === 'multiplication') {
-        document.getElementById('resultMultiplication').innerHTML = "" + operation.mult();
+    if (operand === "multiplication") {
+        document.getElementById("resultMultiplication").innerHTML =
+            "" + operation.mult();
     }
-    if (operand === 'division') {
-        document.getElementById('resultDivision').innerHTML = "" + operation.div();
+    if (operand === "division") {
+        document.getElementById("resultDivision").innerHTML = "" + operation.div();
     }
 };
 // User check typescript excercise
 var userChecker = /** @class */ (function () {
     function userChecker(user) {
         this.user = user;
-        this.adminList = { admin: ['admin', 'marco'], user: ['marco', 'giovanni', 'luca', 'matteo'] };
+        this.adminList = {
+            admin: ["admin", "marco"],
+            user: ["marco", "giovanni", "luca", "matteo"]
+        };
     }
     userChecker.prototype.check = function () {
         var _this = this;
@@ -48,7 +53,6 @@ var userChecker = /** @class */ (function () {
             var role = _a[_i];
             var match = this.adminList[role].some(function (element) { return element === _this.user; });
             if (match) {
-                console.log(roles);
                 roles.push(role);
             }
         }
@@ -57,20 +61,96 @@ var userChecker = /** @class */ (function () {
     return userChecker;
 }());
 var userCheckHandler = function () {
-    var checker = new userChecker(document.getElementById('user').value);
+    var checker = new userChecker(document.getElementById("user").value);
     if (checker.check()) {
         document.getElementById("user-check-result").innerHTML =
-            "<p>The user inserted is" + checker.check() + "</p>";
+            "<p>The user inserted is " + checker.check() + "</p>";
     }
     else {
         document.getElementById("user-check-result").innerHTML =
             "<p>The user inserted is not a user</p>";
     }
 };
-//Inheritance and Polymorphism typescript excercise
-var GeometricFigure = /** @class */ (function () {
-    function GeometricFigure(sides) {
-        this.sides = sides;
+var Circle = /** @class */ (function () {
+    function Circle(h, w) {
+        this.sides = 1;
+        this.h = h;
+        this.w = w;
     }
-    return GeometricFigure;
+    Circle.prototype.print = function () {
+        return {
+            width: this.w + "px",
+            height: this.h + "px",
+            "border-radius": "50%",
+            border: "1px solid black"
+        };
+    };
+    return Circle;
 }());
+var Trapezoid = /** @class */ (function () {
+    function Trapezoid(h, w) {
+        this.sides = 4;
+        this.h = h;
+        this.w = w;
+    }
+    Trapezoid.prototype.print = function () {
+        return {
+            width: this.w + "px",
+            height: this.h + "px",
+            border: '1px solid black',
+            "transform-origin": "50% 50%",
+            transform: "perspective(20px) rotateX(10deg) rotateY(0deg);"
+        };
+    };
+    return Trapezoid;
+}());
+var Rhombus = /** @class */ (function () {
+    function Rhombus(h, w) {
+        this.sides = 4;
+        this.h = h;
+        this.w = w;
+    }
+    Rhombus.prototype.print = function () {
+        return {
+            width: this.w + "px",
+            height: this.h + "px",
+            border: '1px solid black',
+            transform: "rotate(65deg) skewX(45deg) scaleY(cos(45deg))"
+        };
+    };
+    return Rhombus;
+}());
+var geometricFiguresHandler = function () {
+    var figureChoose = document.getElementById("figure")
+        .value;
+    var height = document.getElementById("height").value;
+    var width = document.getElementById("width").value;
+    if (figureChoose === "circle") {
+        var figure = new Circle(+width, +height);
+        var string = "";
+        for (var _i = 0, _a = Object.keys(figure.print()); _i < _a.length; _i++) {
+            var key = _a[_i];
+            string += key + ":" + figure.print()[key] + ";";
+        }
+        document.getElementById("result").style.cssText = string;
+    }
+    else if (figureChoose === "trapezoid") {
+        var figure = new Trapezoid(+width, +height);
+        var string = "";
+        for (var _b = 0, _c = Object.keys(figure.print()); _b < _c.length; _b++) {
+            var key = _c[_b];
+            string += key + ":" + figure.print()[key] + ";";
+        }
+        document.getElementById("result").style.cssText = string;
+    }
+    else if (figureChoose === "rhombus") {
+        var figure = new Rhombus(+width, +height);
+        var string = "";
+        for (var _d = 0, _e = Object.keys(figure.print()); _d < _e.length; _d++) {
+            var key = _e[_d];
+            string += key + ":" + figure.print()[key] + ";";
+        }
+        console.log(string);
+        document.getElementById("result").style.cssText = string;
+    }
+};
--- esercizi-ts/scripts/script.ts
+++ esercizi-ts/scripts/script.ts
@@ -1,114 +1,182 @@
 // 4 operations typescript excercise
 
 interface Operations {
-    num1:number;
-    num2:number;
-    sum: ()=> number;
-    diff: ()=>number;
-    mult: ()=>number;
-    div: ()=>number;
-
+  num1: number;
+  num2: number;
+  sum: () => number;
+  diff: () => number;
+  mult: () => number;
+  div: () => number;
 }
 
-class Operation implements Operations{
+class Operation implements Operations {
+  constructor(public num1: number, public num2: number) {}
 
-    constructor( public num1:number, public num2:number){
-
-    }
-
-    sum(){
-        return this.num1 + this.num2
-    }
-    diff(){
-        return this.num1 - this.num2
-    }
-    mult(){
-        return this.num1 * this.num2
-    }
-    div(){
-        return this.num1 / this.num2
-    }
-
+  sum() {
+    return this.num1 + this.num2;
+  }
+  diff() {
+    return this.num1 - this.num2;
+  }
+  mult() {
+    return this.num1 * this.num2;
+  }
+  div() {
+    return this.num1 / this.num2;
+  }
 }
-
 
 const operationsHandler = (operand) => {
-    let firstNum = prompt("Please, insert the first number");
-    let secondNum = prompt("Please, insert the second number ");
-    let operation = new Operation(+firstNum,+secondNum)
-    if (operand === 'sum'){
-        document.getElementById('resultSum').innerHTML = ""+operation.sum()
-    }
-    if (operand === 'difference'){
-        document.getElementById('resultDifference').innerHTML = ""+operation.diff()
-    }
-    if (operand === 'multiplication'){
-        document.getElementById('resultMultiplication').innerHTML = ""+operation.mult()
-    }
-    if (operand === 'division'){
-        document.getElementById('resultDivision').innerHTML = ""+operation.div()
-    }
-      
-}
+  let firstNum = prompt("Please, insert the first number");
+  let secondNum = prompt("Please, insert the second number ");
+  let operation = new Operation(+firstNum, +secondNum);
+  if (operand === "sum") {
+    document.getElementById("resultSum").innerHTML = "" + operation.sum();
+  }
+  if (operand === "difference") {
+    document.getElementById("resultDifference").innerHTML =
+      "" + operation.diff();
+  }
+  if (operand === "multiplication") {
+    document.getElementById("resultMultiplication").innerHTML =
+      "" + operation.mult();
+  }
+  if (operand === "division") {
+    document.getElementById("resultDivision").innerHTML = "" + operation.div();
+  }
+};
 
 // User check typescript excercise
 
 class userChecker {
+  adminList: object = {
+    admin: ["admin", "marco"],
+    user: ["marco", "giovanni", "luca", "matteo"],
+  };
 
-    adminList:object = {admin:['admin','marco'],user:['marco','giovanni','luca','matteo']}
+  constructor(public user: string) {}
 
-    constructor(public user:string) {
-}
-
-    check(){
-        let roles:string[] = [];
-        for (let role of Object.keys(this.adminList)){
-            let match = this.adminList[role].some(element => element === this.user)
-            if(match){
-                roles.push(role)
-            }
-        }
-        return roles
+  check() {
+    let roles: string[] = [];
+    for (let role of Object.keys(this.adminList)) {
+      let match = this.adminList[role].some((element) => element === this.user);
+      if (match) {
+        roles.push(role);
+      }
     }
-
+    return roles;
+  }
 }
 
 const userCheckHandler = () => {
-    const checker:userChecker = new userChecker((document.getElementById('user') as HTMLInputElement).value)
-    if (checker.check()){
-        document.getElementById("user-check-result").innerHTML =
-      "<p>The user inserted is "+checker.check()+"</p>";
-    } else {
-        document.getElementById("user-check-result").innerHTML =
+  const checker: userChecker = new userChecker(
+    (document.getElementById("user") as HTMLInputElement).value
+  );
+  if (checker.check()) {
+    document.getElementById("user-check-result").innerHTML =
+      "<p>The user inserted is " + checker.check() + "</p>";
+  } else {
+    document.getElementById("user-check-result").innerHTML =
       "<p>The user inserted is not a user</p>";
-    }
-}
+  }
+};
 
 //Inheritance and Polymorphism typescript excercise
 
 interface GeometricFigure {
-    sides:number;
-    h:number;
-    w:number
-}   print: ()=> Object;
-
-class circle implements GeometricFigure {
-
-    sides= 1
-    h:number
-    w:number
-    
-    constructor(h:number,w:number){
-        this.h = h;
-        this.w = w
-    }
-
-    print(){
-        return {
-            width: this.w,
-            height: this.h,
-            'border-radius': '50%',
-        }
-    }
-
+  sides: number;
+  h: number;
+  w: number;
+  print: () => Object;
 }
+
+class Circle implements GeometricFigure {
+  sides = 1;
+  h: number;
+  w: number;
+
+  constructor(h: number, w: number) {
+    this.h = h;
+    this.w = w;
+  }
+
+  print() {
+    return {
+      width: this.w + "px",
+      height: this.h + "px",
+      "border-radius": "50%",
+      border: "1px solid black",
+    };
+  }
+}
+
+class Trapezoid implements GeometricFigure {
+  sides = 4;
+  h: number;
+  w: number;
+
+  constructor(h: number, w: number) {
+    this.h = h;
+    this.w = w;
+  }
+
+  print() {
+    return {
+      width: this.w + "px",
+      height: this.h + "px",
+        border: '1px solid black',
+      "transform-origin": "50% 50%",
+      transform: "perspective(20px) rotateX(10deg) rotateY(0deg);",
+    };
+  }
+}
+class Rhombus implements GeometricFigure {
+  sides = 4;
+  h: number;
+  w: number;
+
+  constructor(h: number, w: number) {
+    this.h = h;
+    this.w = w;
+  }
+
+  print() {
+    return {
+      width: this.w + "px",
+      height: this.h + "px",
+      border:'1px solid black',
+      transform: "rotate(65deg) skewX(45deg) scaleY(cos(45deg))",
+    };
+  }
+}
+
+const geometricFiguresHandler = () => {
+  const figureChoose = (document.getElementById("figure") as HTMLInputElement)
+    .value;
+  const height = (document.getElementById("height") as HTMLInputElement).value;
+  const width = (document.getElementById("width") as HTMLInputElement).value;
+
+  if (figureChoose === "circle") {
+    const figure = new Circle(+width, +height);
+    let string = "";
+    for (let key of Object.keys(figure.print())) {
+      string += key + ":" + figure.print()[key] + ";";
+    }
+    document.getElementById("result").style.cssText = string;
+  } else if (figureChoose === "trapezoid") {
+    const figure = new Trapezoid(+width, +height);
+    let string = "";
+    for (let key of Object.keys(figure.print())) {
+      string += key + ":" + figure.print()[key] + ";";
+    }
+    document.getElementById("result").style.cssText = string;
+  } else if (figureChoose === "rhombus") {
+    const figure = new Rhombus(+width, +height);
+    let string = "";
+    for (let key of Object.keys(figure.print())) {
+      string += key + ":" + figure.print()[key] + ";";
+    }
+    console.log(string);
+    document.getElementById("result").style.cssText = string;
+  }
+};
